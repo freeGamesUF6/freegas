cc098fd11c8d9476e4b064c9b5850d3240e29287
avalcarcel <avalcarc@jaumebalmes.net>
2017-05-05 08:58:59 +0200

    Update Game.java

--- src/main/java/com/freegames/model/Game.java
+++ src/main/java/com/freegames/model/Game.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package com.freegames.model;
-
+import java.util.Set;
 import java.io.Serializable;
 import java.util.Set;
 import javax.persistence.Entity;
