a037fff660811a69bd9b6b095f3b32a030a5e809
avalcarcel <avalcarc@jaumebalmes.net>
2017-05-05 09:00:12 +0200

    Update GameDAO.java

--- src/main/java/com/freegames/model/GameDAO.java
+++ src/main/java/com/freegames/model/GameDAO.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package com.freegames.model;
-
+import java.util.Set;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
